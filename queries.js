500e7c525e3b1f19d65f694e03b90c037e5f0d8a
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-13 22:22:53 +0100

    Added endpoints for update and delete user

--- queries.js
+++ queries.js
@@ -42,3 +42,30 @@ export const createUser = (request, response) => {
         response.status(201).send(`User added with ID: ${results.insertId}`)
     })
 }
+
+export const updateUser = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { name, email } = request.body;
+
+    pool.query(
+        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+        [name, email, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+
+export const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('DELECT FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`User deleted with ID: ${id}`)
+    })
+}
