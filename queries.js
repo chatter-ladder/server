632f77cc96afe68db453aee7c7ef62a9f73b064a
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-14 15:02:44 +0100

    Endpoint post vocabulary now includes query to update vocabulary table in db

--- queries.js
+++ queries.js
@@ -80,21 +80,28 @@ export const getVocabulary = (request, response) => {
 
 export const createVocabulary = (request, response) => {
     const id = parseInt(request.params.id);
-    const { word, word_language, translation, translation_language } = request.body;
+    const { user_id, word, word_language, translation, translation_language } = request.body;
 
-    pool.query('INSERT INTO words (word, language) VALUES ($1, $2);', [word, word_language], (error, results) => {
+    pool.query('INSERT INTO words (word, language) VALUES ($1, $2) RETURNING id;', [word, word_language], (error, result) => {
         if (error) {
             throw error
         }
+        const word_id = result.rows[0].id;
 
-        pool.query('INSERT INTO translations (translation, language) VALUES ($1, $2);', [translation, translation_language], (error, results) => {
+        pool.query('INSERT INTO translations (translation, language) VALUES ($1, $2) RETURNING id;', [translation, translation_language], (error, result) => {
             if (error) {
                 throw error
             }
-        response.sendStatus(201)
-        // response.status(201).send(`Translation was added with ID: ${results}`)
-    })
+            const translation_id = result.rows[0].id;
 
+            pool.query('INSERT INTO vocabulary (user_id, word_id, translation_id, progress, num_seen, num_correct) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;', [user_id, word_id, translation_id, 'new', 0, 0], (error, result) => {
+                if (error) {
+                    throw error
+                }
+                response.sendStatus(201)
+            })
+        // response.status(201).send(`Translation was added with ID: ${results}`)
+        })
     })
 
 }
