9f8f3dbe7b7a6c6b6ffbfa48a85cd387315266ea
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-20 15:06:20 +0100

    Updated getFlashcards to send number seen and number correct

--- queries.js
+++ queries.js
@@ -125,10 +125,8 @@ export const createVocabulary = (request, response) => {
 }
 
 export const getFlashcards = (request, response) => {
-    const id = parseInt(request.params.id);
-    const { numFlashcards } = request.body;
-
-    pool.query('SELECT w.word, t.translation, v.progress FROM vocabulary AS v LEFT JOIN words AS w ON v.word_id = w.id LEFT JOIN translations AS t ON v.translation_id = t.id WHERE user_id = $1 ORDER BY random() LIMIT $2;', [id, numFlashcards], (error, results) => {
+    const { user_id, number } = request.body;
+    pool.query('SELECT w.word, t.translation v.num_seen v.num_correct v.progress FROM vocabulary AS v LEFT JOIN words AS w ON v.word_id = w.id LEFT JOIN translations AS t ON v.translation_id = t.id WHERE user_id = $1 ORDER BY random() LIMIT $2;', [user_id, number], (error, results) => {
         if (error) {
             throw error
         }
