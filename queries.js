0fd464346be46020e9b83b4417d1eca773a71b7d
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-20 15:08:03 +0100

    Fixed typo in getFlashcards query

--- queries.js
+++ queries.js
@@ -126,7 +126,7 @@ export const createVocabulary = (request, response) => {
 
 export const getFlashcards = (request, response) => {
     const { user_id, number } = request.body;
-    pool.query('SELECT w.word, t.translation v.num_seen v.num_correct v.progress FROM vocabulary AS v LEFT JOIN words AS w ON v.word_id = w.id LEFT JOIN translations AS t ON v.translation_id = t.id WHERE user_id = $1 ORDER BY random() LIMIT $2;', [user_id, number], (error, results) => {
+    pool.query('SELECT w.word, t.translation, v.num_seen, v.num_correct, v.progress FROM vocabulary AS v LEFT JOIN words AS w ON v.word_id = w.id LEFT JOIN translations AS t ON v.translation_id = t.id WHERE user_id = $1 ORDER BY random() LIMIT $2;', [user_id, number], (error, results) => {
         if (error) {
             throw error
         }
