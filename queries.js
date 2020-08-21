246277d3bcd3fae6a2c42dc79a60146970a95881
Karla Gardiner <karla.gardiner@gmail.com>
2020-08-21 12:27:12 +0100

    Query added for endpoint flashcards/complete

--- queries.js
+++ queries.js
@@ -135,5 +135,22 @@ export const getFlashcards = (request, response) => {
 }
 
 export const updateVocabulary = (request, response) => {
-    console.log(request.body[0])
+    const vocabList = request.body
+
+    vocabList.map( vocab => {
+        let isSeen = 0
+        let isCorrect = 0
+        if (vocab.seen) {
+            isSeen = 1
+        }
+        if (vocab.correct && vocab.seen) {
+            isCorrect = 1
+        }
+        pool.query('UPDATE vocabulary SET num_seen = num_seen + $1, num_correct = num_correct + $2, last_seen_at = now() WHERE id = $3;', [isSeen, isCorrect, vocab.id], (error, results) => {
+            if (error) {
+                throw error
+            }
+        })
+    })
+    response.status(200)
 }
