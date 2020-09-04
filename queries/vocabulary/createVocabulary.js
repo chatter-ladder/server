ea7c53c308dbec807d3006d35504a1f0d1b8822f
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-04 12:09:54 +0100

    Updated countVocabulary query to use the user id from authenticated token

--- queries/vocabulary/createVocabulary.js
+++ queries/vocabulary/createVocabulary.js
@@ -1,6 +1,11 @@
 import pool from "../../db/connection.js";
 
 export const createVocabulary = (request, response) => {
+  // TODO:
+  // 1. check if word already exists in db. If so, retrieve word_id, if not create new
+  // 2. check if translation already exists in db. If so, retrieve translation_id, if not create new
+  // 3. check if word (& language?, & translation?) already exist in user's list. If so, sent error back to user
+
   const user_id = request.user.id;
   const {
     word,
@@ -11,8 +16,8 @@ export const createVocabulary = (request, response) => {
 
   pool.query(
     `INSERT INTO words (word, language) 
-    VALUES ($1, $2) 
-    RETURNING id;`,
+        VALUES ($1, $2) 
+        RETURNING id;`,
     [word, word_language],
     (error, result) => {
       if (error) {
@@ -22,8 +27,8 @@ export const createVocabulary = (request, response) => {
 
       pool.query(
         `INSERT INTO translations (translation, language) 
-        VALUES ($1, $2) 
-        RETURNING id;`,
+            VALUES ($1, $2) 
+            RETURNING id;`,
         [translation, translation_language],
         (error, result) => {
           if (error) {
@@ -33,8 +38,8 @@ export const createVocabulary = (request, response) => {
 
           pool.query(
             `INSERT INTO vocabulary (user_id, word_id, translation_id, progress, num_seen, num_correct) 
-            VALUES ($1, $2, $3, $4, $5, $6) 
-            RETURNING id;`,
+                VALUES ($1, $2, $3, $4, $5, $6) 
+                RETURNING id;`,
             [user_id, word_id, translation_id, "new", 0, 0],
             (error, result) => {
               if (error) {
