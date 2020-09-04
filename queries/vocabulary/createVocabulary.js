ee0a9fc3345909051b295943f957ffc2f20b7670
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-04 11:34:26 +0100

    Updated createVocabulary query to use the user id from authenticated token

--- queries/vocabulary/createVocabulary.js
+++ queries/vocabulary/createVocabulary.js
@@ -1,28 +1,44 @@
-import pool from '../../db/connection.js';
+import pool from "../../db/connection.js";
 
 export const createVocabulary = (request, response) => {
-    const id = parseInt(request.params.id);
-    const { user_id, word, word_language, translation, translation_language } = request.body;
+  const user_id = request.user.id;
+  const {
+    word,
+    word_language,
+    translation,
+    translation_language,
+  } = request.body;
 
-    pool.query('INSERT INTO words (word, language) VALUES ($1, $2) RETURNING id;', [word, word_language], (error, result) => {
-        if (error) {
-            throw error
-        }
-        const word_id = result.rows[0].id;
+  pool.query(
+    "INSERT INTO words (word, language) VALUES ($1, $2) RETURNING id;",
+    [word, word_language],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      const word_id = result.rows[0].id;
 
-        pool.query('INSERT INTO translations (translation, language) VALUES ($1, $2) RETURNING id;', [translation, translation_language], (error, result) => {
-            if (error) {
-                throw error
+      pool.query(
+        "INSERT INTO translations (translation, language) VALUES ($1, $2) RETURNING id;",
+        [translation, translation_language],
+        (error, result) => {
+          if (error) {
+            throw error;
+          }
+          const translation_id = result.rows[0].id;
+
+          pool.query(
+            "INSERT INTO vocabulary (user_id, word_id, translation_id, progress, num_seen, num_correct) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;",
+            [user_id, word_id, translation_id, "new", 0, 0],
+            (error, result) => {
+              if (error) {
+                throw error;
+              }
+              response.sendStatus(201);
             }
-            const translation_id = result.rows[0].id;
-
-            pool.query('INSERT INTO vocabulary (user_id, word_id, translation_id, progress, num_seen, num_correct) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;', [user_id, word_id, translation_id, 'new', 0, 0], (error, result) => {
-                if (error) {
-                    throw error
-                }
-                response.sendStatus(201)
-            })
-        })
-    })
-
-}
+          );
+        }
+      );
+    }
+  );
+};
--- queries/vocabulary/updateVocabulary.js
+++ queries/vocabulary/updateVocabulary.js
@@ -1,22 +1,28 @@
-import pool from '../../db/connection.js';
+import pool from "../../db/connection.js";
 
 export const updateVocabulary = (request, response) => {
-    const vocabList = request.body
+  console.log(request);
+  const vocabList = request.body;
+  const user = request.user;
 
-    vocabList.map( vocab => {
-        let isSeen = 0
-        let isCorrect = 0
-        if (vocab.seen) {
-            isSeen = 1
+  vocabList.map((vocab) => {
+    let isSeen = 0;
+    let isCorrect = 0;
+    if (vocab.seen) {
+      isSeen = 1;
+    }
+    if (vocab.correct && vocab.seen) {
+      isCorrect = 1;
+    }
+    pool.query(
+      "UPDATE vocabulary SET num_seen = num_seen + $1, num_correct = num_correct + $2, last_seen_at = now() WHERE id = $3;",
+      [isSeen, isCorrect, user.id],
+      (error, results) => {
+        if (error) {
+          throw error;
         }
-        if (vocab.correct && vocab.seen) {
-            isCorrect = 1
-        }
-        pool.query('UPDATE vocabulary SET num_seen = num_seen + $1, num_correct = num_correct + $2, last_seen_at = now() WHERE id = $3;', [isSeen, isCorrect, vocab.id], (error, results) => {
-            if (error) {
-                throw error
-            }
-        })
-    })
-    response.status(200)
-}
+      }
+    );
+  });
+  response.status(200);
+};
